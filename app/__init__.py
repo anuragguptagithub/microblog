8e443bfa4fa5f169eb9adcd542c6a6855899f574
anurag <pragsmatic@gmail.com>
2019-04-28 19:46:27 +0200

    pagination

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-import logging, os
-from logging.handlers import SMTPHandler, RotatingFileHandler
-
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,8 +13,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-
-from app import routes, models, errors
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -43,3 +41,5 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+from app import routes, models, errors
